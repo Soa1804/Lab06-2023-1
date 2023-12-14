c294afb62244d3a8fe50aee4c2f76bab09900791
Soa1804 <137106233+Soa1804@users.noreply.github.com>
2023-12-14 20:00:04 +0000

    commit 14-12

--- src/main/ordenadores/OrdenarPorContagemSimples.java
+++ src/main/ordenadores/OrdenarPorContagemSimples.java
@@ -4,7 +4,29 @@ public class OrdenarPorContagemSimples implements Ordenador{
 
     @Override
     public int[] ordene(int[] array) {
+        if(array == null || array.length<=1){
+            return array;
+        }
 
+        int valMax = array[0];
+        for(int v = 1; v<array.length;v++){
+            if(array[v]> valMax){
+                valMax = array[v];
+            }
+        }
+        int[] contador = new int[valMax+1];
+
+        for(int v: array){
+            contador[v]++;
+        }
+
+        int indice = 0;
+        for(int v=0; v< contador.length;v++){
+            while(contador[v]>0){
+                array[indice++]= v;
+                contador[v]--;
+            }
+        }
         return array;
     }
     
--- src/main/ordenadores/OrdenarQuick.java
+++ src/main/ordenadores/OrdenarQuick.java
@@ -5,10 +5,26 @@ public class OrdenarQuick implements Ordenador{
     public int[] ordene(int[] array) {
 
         return array;
+
     }
 
     public int quickParticio(int[] array, int inicio, int fim){
-        return 0;
+        int pivo = array[fim];
+        int valor = inicio - 1;
+
+        for(int k = inicio; k<fim; k++){
+            if(array[k]<= pivo){
+                valor++;
+
+                int sec = array[valor+1];
+                array[valor+1] = array[fim];
+                array[fim] = sec;
+            }
+        }
+        int sec = array[valor+1];
+        array[valor+1] = array[fim];
+        array[fim] = sec;
+        return valor;
     }
     
 }
